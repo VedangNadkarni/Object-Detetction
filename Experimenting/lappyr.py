fb4c562f397616080acc6549e0d815e760c026a8
VedangNadkarni <77508780+VedangNadkarni@users.noreply.github.com>
2021-11-24 14:51:23 +0530

    creating an array to store and manipualte trackbar position

--- Experimenting/lappyr.py
+++ Experimenting/lappyr.py
@@ -57,13 +57,14 @@ def main():
     cap = video.create_capture(fn)
 
     leveln = 6
+    trackbarptr = [5 for i in range(leveln)]
     cv.namedWindow('level control')
     for i in xrange(leveln):
-        cv.createTrackbar('%d'%i, 'level control', 5, 50, nothing)
+        cv.createTrackbar('%d'%i, 'level control',trackbarptr[i], 50, nothing)
 
     while True:
         _ret, frame = cap.read()
-
+        frame2 = frame
         pyr = build_lappyr(frame, leveln)
         for i in xrange(leveln):
             v = int(cv.getTrackbarPos('%d'%i, 'level control') / 5)
@@ -84,10 +85,10 @@ def main():
         
         # cv.imshow("edged1", edged)
         # cv.imshow("edged3", edged)
-        # kernel2 = np.array([[-1,-1,-1], 
-        #                [-1, 9,-1],
-        #                [-1,-1,-1]])
-        # edged = cv.filter2D(edged, -1, kernel2) # applying the sharpening kernel to the input image & displaying it.
+        kernel2 = np.array([[-1,-1,-1], 
+                       [-1, 9,-1],
+                       [-1,-1,-1]])
+        edged = cv.filter2D(edged, -1, kernel2) # applying the sharpening kernel to the input image & displaying it.
         cnts = cv.findContours(edged, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         cnts = sorted(cnts, key=cv.contourArea, reverse=True)
@@ -117,8 +118,43 @@ def main():
         k = cv.waitKey(30)
         if k == 27:
             return
+            
         ###modified ends
+        
+        '''
+        ###modified 2
+        
+        pyr2 = build_lappyr(edged, leveln)
+        for i in xrange(leveln):
+            v = int(cv.getTrackbarPos('%d'%i, 'level control') / 5)
+            pyr2[i] *= v
+        res2 = merge_lappyr(pyr2)
+        cv.imshow('laplacian pyramid filter2', res2)
 
+        pil_image2 = Image.fromarray(res2.astype(np.uint8))
+        # pil_image = Image.open('Image.jpg').convert('RGB') 
+        open_cv_image2 = np.array(pil_image2)
+        open_cv_image2 = open_cv_image2[:, ::-1].copy()
+        # edged2 = cv.cvtColor(open_cv_image2, cv.COLOR_BGR2GRAY)
+        edged2 = open_cv_image2
+        cnts2 = cv.findContours(edged2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cnts2 = imutils.grab_contours(cnts2)
+        cnts2 = sorted(cnts2, key=cv.contourArea, reverse=True)
+        screenCnt2 = []
+        for c in cnts:
+            peri = cv.arcLength(c, True)
+            approx = cv.approxPolyDP(c, 0.03*peri, True)
+
+            if len(approx) == 7:
+                screenCnt.append(approx)
+                
+        if screenCnt is not []:
+            cv.drawContours(frame2, screenCnt2, -1, (0, 255, 0), 3)
+            cv.imshow("Arrow2", frame)
+            cv.imshow("edged2", edged2)
+
+        ###modified 2 ends
+        '''
         if cv.waitKey(1) == 27:
             break
 
